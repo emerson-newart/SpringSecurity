69268182fe13489f3687d6b451ad897046241e03
emerson-newart <emerson.art.n@hotmail.com>
2024-02-07 09:54:00 -0300

    Criando rota GetMapping - para listar os produtos

--- src/main/java/br/com/curso/controllers/ProductController.java
+++ src/main/java/br/com/curso/controllers/ProductController.java
@@ -4,14 +4,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.curso.domain.product.Product;
 import br.com.curso.domain.product.ProductRequestDTO;
+import br.com.curso.domain.product.ProductResponseDTO;
 import br.com.curso.repository.ProductRepository;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -26,6 +33,12 @@ public class ProductController {
         this.repository.save(newProduct);
         
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+      List<ProductResponseDTO> productList = this.repository.findAll().stream().map(ProductResponseDTO::new).toList();
+      return ResponseEntity.ok(productList);
     }
     
 }
